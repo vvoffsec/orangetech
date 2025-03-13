d7ce38aaba457168a3d54450a71cee6fdada697d
Vishal Venkateswaran <113947350+vvoffsec@users.noreply.github.com>
2025-03-13 16:00:00 -0700

    Created env file to hide api keys and auth domains. Deleted unneeded files.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD84B8v8_aVvD-OBccLQ8BFKkArNGwlUJ0",
-    authDomain: "orangetech-a2c45.firebaseapp.com",
-    projectId: "orangetech-a2c45",
-    storageBucket: "orangetech-a2c45.firebasestorage.app",
-    messagingSenderId: "391519036830",
-    appId: "1:391519036830:web:47ce30bff81091a63e228f",
-    measurementId: "G-V9TK0L8PWF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
